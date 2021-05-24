0f253c2fe90a0a46cb484125c3940676ad319bc0
neha6281 <84026801+neha6281@users.noreply.github.com>
2021-05-24 20:31:25 +0530

    positive numbers

---
+++ positive numbers.py
@@ -0,0 +1,14 @@
+#using for loop
+list1 = [5,-3,0,23,-6,8]
+for num  in list1:
+  if num >=0:
+    print (num,end = " ")
+    
+    
+#using while loop 
+list1 = [5,-3,0,23,-6,8]
+num = 0
+while (num < len(list1)):
+  if list1[num] >=0:
+      print(list1[num],end = " ")
+  num += 1
